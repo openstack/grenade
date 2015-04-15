5e7f2d0880510264b1b09d9b8527533b1132fc29
Sean Dague <sean@dague.net>
2015-04-15 08:10:00 -0400

    Add cinder modular verification
    This includes a boot from volume test that includes putting state
    inside a volume and ensuring that it's there later.
    
    Depends-On: Iefbc8d06eb677740956b89e157e8c9e176a0564e
    
    ... which removes stopping of tgt service on stop_cinder
    
    Change-Id: I28fb72c978e7f4721bd694fa9cbbac19216c0b6f

---
+++ projects/70_cinder/resources.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+#
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+set -o errexit
+
+source $GRENADE_DIR/grenaderc
+source $GRENADE_DIR/functions
+
+source $TOP_DIR/openrc admin admin
+
+set -o xtrace
+
+CINDER_USER=cinder_grenade
+CINDER_PROJECT=cinder_grenade
+CINDER_PASS=pass
+CINDER_SERVER=cinder_server1
+CINDER_KEY=cinder_key
+CINDER_KEY_FILE=$SAVE_DIR/cinder_key.pem
+CINDER_VOL=cinder_grenade_vol
+# don't put ' or " in this, it complicates things
+CINDER_STATE="I am a teapot"
+CINDER_STATE_FILE=verify.txt
+
+DEFAULT_INSTANCE_TYPE=${DEFAULT_INSTANCE_TYPE:-m1.tiny}
+DEFAULT_IMAGE_NAME=${DEFAULT_IMAGE_NAME:-cirros-0.3.2-x86_64-uec}
+
+# BUG openstack client doesn't work with cinder v2
+export OS_VOLUME_API_VERSION=1
+
+function _cinder_set_user {
+    OS_TENANT_NAME=$CINDER_PROJECT
+    OS_USERNAME=$CINDER_USER
+    OS_PASSWORD=$CINDER_PASS
+}
+
+function create {
+    # creates a tenant for the server
+    eval $(openstack project create -f shell -c id $CINDER_PROJECT)
+    if [[ -z "$id" ]]; then
+        die $LINENO "Didn't create $CINDER_PROJECT project"
+    fi
+    resource_save cinder project_id $id
+
+    # creates the user, and sets $id locally
+    eval $(openstack user create $CINDER_USER \
+        --project $id \
+        --password $CINDER_PASS \
+        -f shell -c id)
+    if [[ -z "$id" ]]; then
+        die $LINENO "Didn't create $CINDER_USER user"
+    fi
+    resource_save cinder user_id $id
+
+    # set ourselves to the created cinder user
+    _cinder_set_user
+
+    # setup a working security group
+    # BUG(sdague): I have no idea how to make openstack security group work
+    # openstack security group create --description "BUG" $CINDER_USER
+    # openstack security group rule create --proto icmp --dst-port 0 $CINDER_USER
+    # openstack security group rule create --proto tcp --dst-port 22 $CINDER_USER
+    nova secgroup-create $CINDER_USER "BUG: this should not be mandatory"
+    nova secgroup-add-rule $CINDER_USER icmp -1 -1 0.0.0.0/0
+    nova secgroup-add-rule $CINDER_USER tcp 22 22 0.0.0.0/0
+
+    # create key pairs for access
+    openstack keypair create $CINDER_KEY > $CINDER_KEY_FILE
+    chmod 600 $CINDER_KEY_FILE
+
+    # Create a bootable volume
+    eval $(openstack volume create --image $DEFAULT_IMAGE_NAME --size 1 $CINDER_VOL -f shell)
+    resource_save cinder cinder_volume_id $id
+
+    # BUG: openstack client doesn't support --wait on volumes, so loop
+    # and wait until it's bootable. This typically only takes a second
+    # or two.
+    local timeleft=30
+    while [[ $timeleft -gt 0 ]]; do
+        eval $(openstack volume show cinder_grenade_vol -f shell -c bootable)
+        if [[ "$bootable" != "true" ]]; then
+            echo "Volume is not yet bootable, waiting..."
+            sleep 1
+            timeleft=$((timeleft - 1))
+            if [[ $timeleft == 0 ]]; then
+                die $LINENO "Volume failed to become bootable"
+            fi
+        else
+            break
+        fi
+    done
+
+    # work around for neutron because there is no such thing as a default
+    local net_id=$(resource_get network net_id)
+    if [[ -n "$net_id" ]]; then
+        local net="--nic net-id=$net_id"
+    fi
+
+    # Boot from this volume
+    openstack server create --volume $id \
+        --flavor $DEFAULT_INSTANCE_TYPE \
+        --security-group $CINDER_USER \
+        --key-name $CINDER_KEY \
+        $net \
+        $CINDER_SERVER --wait
+
+    # Add a floating IP because this is something which will work in
+    # either n-net or neutron.
+    eval $(openstack ip floating create public -f shell -c id -c ip)
+    resource_save cinder cinder_server_ip $ip
+    resource_save cinder cinder_server_float $id
+    openstack ip floating add $ip $CINDER_SERVER
+
+    # ping check on the way up so we can add ssh content
+    ping_check_public $ip 30
+
+    # turn of errexit for this portion of the retry
+    set +o errexit
+    local timeleft=30
+    while [[ $timeleft -gt 0 ]]; do
+        ssh -o "StrictHostKeyChecking no" -i $CINDER_KEY_FILE cirros@$ip \
+            "echo '$CINDER_STATE' > $CINDER_STATE_FILE"
+        local rc=$?
+
+        if [[ "$rc" -ne 0 ]]; then
+            echo "SSH not responding yet, trying again..."
+            sleep 1
+            timeleft=$((timeleft - 1))
+            if [[ $timeleft == 0 ]]; then
+                die $LINENO "SSH to the client did not work, something very wrong"
+            fi
+        else
+            break
+        fi
+    done
+}
+
+function verify {
+    # just call verify_noapi, as that's an actual resource survival test
+    verify_noapi
+}
+
+function verify_noapi {
+    local server_ip=$(resource_get cinder cinder_server_ip)
+    ping_check_public $server_ip 30
+    # this sync is here to ensure that we don't accidentally pass when
+    # the volume is actually down.
+    timeout 30 ssh -o "StrictHostKeyChecking no" -i $CINDER_KEY_FILE cirros@$server_ip \
+        "sync"
+    local state=$(ssh -o "StrictHostKeyChecking no" -i $CINDER_KEY_FILE cirros@$server_ip \
+        "cat $CINDER_STATE_FILE")
+    if [[ "$state" != "$CINDER_STATE" ]]; then
+        die $LINENO "The expected state left in the volume isn't there!"
+    fi
+    echo "Cinder verify found the expected state file: SUCCESS!"
+}
+
+function destroy {
+    _cinder_set_user
+    openstack ip floating remove $(resource_get cinder cinder_server_ip) $CINDER_SERVER
+    openstack ip floating delete $(resource_get cinder cinder_server_float)
+
+    openstack server delete $CINDER_SERVER
+    # wait for server to be down before we delete the volume
+    local wait_cmd="while openstack server show $CINDER_SERVER >/dev/null; do sleep 1; done"
+    timeout 30 sh -c "$wait_cmd"
+
+    openstack volume delete $CINDER_VOL
+
+    nova secgroup-delete $CINDER_USER
+
+    # lastly, get rid of our user - done as admin
+    source $TOP_DIR/openrc admin admin
+    local user_id=$(resource_get cinder user_id)
+    local project_id=$(resource_get cinder project_id)
+    openstack user delete $user_id
+    openstack project delete $project_id
+}
+
+# Dispatcher
+case $1 in
+    "create")
+        create
+        ;;
+    "verify_noapi")
+        verify_noapi
+        ;;
+    "verify")
+        verify
+        ;;
+    "destroy")
+        destroy
+        ;;
+    "force_destroy")
+        set +o errexit
+        destroy
+        ;;
+esac
