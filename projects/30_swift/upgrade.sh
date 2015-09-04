5a40502b465807c96e116eca01ac7bc19db35826
Sean Dague <sean@dague.net>
2015-09-04 07:48:29 -0400

    mount xfs with nouuid
    There is a small but non zero race in XFS mounting filesystems that
    then error with duplicate UUID. We can mount with this nouuid option
    to avoid this.
    
    Change-Id: Ib558459681157d7748b2e001f061859109b43bc0
    Closes-Bug: #1492235

--- projects/30_swift/upgrade.sh
+++ projects/30_swift/upgrade.sh
@@ -70,7 +70,7 @@ rm -f $SWIFT_AUTH_CACHE_DIR/*
 
 # Mount backing disk
 if ! egrep -q ${SWIFT_DATA_DIR}/drives/sdb1 /proc/mounts; then
-    sudo mount -t xfs -o loop,noatime,nodiratime,nobarrier,logbufs=8  \
+    sudo mount -t xfs -o nouuid,loop,noatime,nodiratime,nobarrier,logbufs=8  \
         ${SWIFT_DATA_DIR}/drives/images/swift.img ${SWIFT_DATA_DIR}/drives/sdb1
 fi
 
