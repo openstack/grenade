216a1e52fc2e87c7ccba7b2f39420a0f6240c6f6
Eric Fried <openstack@fried.cc>
2020-01-21 11:40:23 -0600

    Make docs build again
    Docs build was broken with:
    
    TypeError: __init__() got an unexpected keyword argument 'handle_io_errors'
    
    Upgrade sphinx requirements to those of a recent decade, and replace
    the oslosphinx extension and requirement with openstackdocstheme.
    
    At the same time, split the deps for the docs build out of
    test-requirements.txt and put them in docs/requiremenst.txt as the gods
    intended.
    
    Change-Id: I6f82b5c37217ca2bdcf85c1ce07293b3d1026370
    (cherry picked from commit 0d5042b0ce5b402ec023b0f6c8c226a3e02f30ca)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ def setup(app):
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'oslosphinx' ]
+extensions = ['openstackdocstheme']
 
 todo_include_todos = True
 
