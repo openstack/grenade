af1b4bf7d6a0bc7f8c89ca33bff534d6c776aa2a
Martin Kopec <mkopec@redhat.com>
2021-04-27 15:05:32 +0000

    Remove neutron workaround for mitaka and newton
    Remove condition for checking if BASE_DEVSTACK_DIR/lib/neutron
    exists which was introduced ~5 years ago.
    Based on the comment it looks like it can be deleted.
    
    Change-Id: Ice9a709ad75bb5794aebed379fb35f78f0b3422f

--- projects/50_neutron/shutdown.sh
+++ projects/50_neutron/shutdown.sh
@@ -12,12 +12,7 @@ source $BASE_DEVSTACK_DIR/functions
 source $BASE_DEVSTACK_DIR/stackrc # needed for status directory
 source $BASE_DEVSTACK_DIR/lib/apache
 source $BASE_DEVSTACK_DIR/lib/tls
-# TODO(sdague): remove this conditional once we've branched
-# grenade. Right now we need to support stable/mitaka, stable/newton,
-# and master devstack
-if [[ -e $BASE_DEVSTACK_DIR/lib/neutron ]]; then
-    source $BASE_DEVSTACK_DIR/lib/neutron
-fi
+source $BASE_DEVSTACK_DIR/lib/neutron
 source $BASE_DEVSTACK_DIR/lib/neutron-legacy
 
 set -o xtrace
